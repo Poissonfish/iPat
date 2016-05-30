d8a68a09fd50b2e05b072dea062c333c3b1ecb9d
Poissonfish <chun-peng.chen@wsu.edu>
2016-05-30 23:11:20 +0800

    暫時拖曳成功 但是會有空格間隙

--- src/main/iPat.java
+++ src/main/iPat.java
@@ -16,109 +16,138 @@ public class iPat {
 	public static void main(String[] args){        
 		JFrame main = new JFrame();
 		main.setTitle("iPat");	
-		main.setSize(400, 300);
+		main.setSize(2000,600);
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container cPane = main.getContentPane();
 		cPane.add(new myPanel());	
 		main.setVisible(true);
 	}
 }
-class myPanel extends JPanel implements MouseMotionListener{
+
+class myPanel extends JPanel implements MouseMotionListener{	
+	private static final int MAX=10;
+	int[] imageX= new int[MAX];
+	int[] imageY= new int[MAX];
+	int imageH;
+	int imageW;
 	
-	int imageX[];
-	int imageY[];
-	Image tbg[];
-	Image ttt;
-	Rectangle imageBounds[];
-			
-			
+	Image[] ttt= new Image[MAX];
+	Rectangle[] imageBounds= new Rectangle[MAX];
+		
 	int TBindex =0;
-	int Click=1;
+	int count=0;
+	Boolean Create=false;
 	
 	JButton TB = new JButton();
-	JPanel buPanel = new JPanel(new MigLayout("debug"));
-	
+	JPanel buPanel = new JPanel(new MigLayout("debug"));	
 
 	@Override
 	protected void paintComponent(Graphics g) {
 	     super.paintComponent(g);
+		 System.out.println("use paint");
 	     //Graphics2D g2D = (Graphics2D) g;
-	     g.drawImage(tbg[TBindex], imageX[TBindex], imageY[TBindex], this);	     
+	     if (Create){	    	 
+	    	 for (int i=1; i<=count; i++){
+			     g.drawImage(ttt[i], imageX[i], imageY[i], this);	
+				 System.out.println(i);
+	    	 }
+	     }
 	}
 	
 	@Override
 	public void mouseMoved(MouseEvent ev) {
-		int move_x=ev.getX();
-		int move_y=ev.getY();
-		
-		for (int i=0;i<10;i++){
-			if (imageBounds[i].contains(move_x, move_y)){
-				TBindex=i;
-			}
-		}
 	}
-	
-	
-	
 	
 	@Override
 	 public void mouseDragged(MouseEvent e) {
 		int imX = e.getX();
 		int imY = e.getY();
-		Graphics graphics = getGraphics();
- 		graphics.setXORMode(getBackground());
- 		((Graphics2D) graphics).drawImage(tbg[TBindex], imageX[TBindex], imageY[TBindex], this);
- 		((Graphics2D) graphics).drawImage(tbg[TBindex], imX, imY, this);
- 		graphics.dispose();
+		if (TBindex !=0){
+			Graphics graphics = getGraphics();
+		
+	 		graphics.setXORMode(getBackground());
+	 		((Graphics2D) graphics).drawImage(ttt[TBindex], imageX[TBindex], imageY[TBindex], this);
+	 		
+			imageBounds[TBindex]=new Rectangle(imX, imY,ttt[TBindex].getWidth(null), ttt[TBindex].getHeight(null));
+			
+			imageX[TBindex]=imX;
+		 	imageY[TBindex]=imY;
+			((Graphics2D) graphics).drawImage(ttt[TBindex], imageX[TBindex], imageY[TBindex], this);		
+	 		
+			graphics.dispose();
+
+		}	
 	}
 	
 	
 	public myPanel(){	
+		
 		JPanel mainPanel= new JPanel(new MigLayout("debug"));
-
 		try{
-			Image iconPS = ImageIO.read(getClass().getResource("../resources/Model.png"));
+			Image iconPS = ImageIO.read(this.getClass().getResourceAsStream("Model.png"));
 			TB.setIcon(new ImageIcon(iconPS));
 		} catch (IOException ex){}
-		//for (int i=0; i<11; i++){
+		
+		for (int i=1; i<=MAX-1; i++){
 			try{
-				//tbg[0] = ImageIO.read(getClass().getResource("../resources/Figure.png"));
-				ttt = ImageIO.read(getClass().getResource("../resources/Figure.png"));
+				ttt[i] = ImageIO.read(this.getClass().getResourceAsStream("Figure.png"));
+			} catch (IOException ex){System.out.println("file not found!");}
+		}
+		int imageH=ttt[1].getHeight(null);	
+		int imageW=ttt[1].getWidth(null);				
 
-			} catch (IOException ex){}
-						
 		//}
-	
-
+		
+		this.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mousePressed(MouseEvent ee){
+				int move_x=ee.getX();
+    			int move_y=ee.getY();			
+    			if (Create){			
+    				for (int i=1; i<=count;i++){
+    					if (imageBounds[i].contains(move_x, move_y)){
+    						TBindex=i;
+    						System.out.println(i);
+    					}
+    				}
+    			}
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent ee){
+				TBindex=0;
+			}
+		});
 		
 		TB.addMouseListener(new MouseAdapter() {
     		@Override
     		public void mousePressed(MouseEvent evt) {
     			int x = evt.getX();
     			int y = evt.getY();
-    			if (contains(x,y)){ 
-    				imageX[TBindex]=0;
-    				imageY[TBindex]=0;
-    				imageBounds[TBindex]=new Rectangle(imageX[TBindex], imageY[TBindex],150, 150);
+    			if (contains(x,y)){
+    				count++; 
+    				Create=true;
+    				imageX[count]=50+200*(count-1);
+    				imageY[count]=300;		
+    				imageBounds[count]=new Rectangle(imageX[count], imageY[count],ttt[count].getHeight(null), ttt[count].getWidth(null));
     				repaint();
-    				TBindex++;
     			}
+    				
     		}
     	});   	
-		
+		/*
 		mainPanel.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mousePressed(MouseEvent evp){
-				Click= -1;
+				//Click= -1;
 			}
 			@Override
 			public void mouseReleased(MouseEvent evr){
-				Click= 1;
+				//Click= 1;
 			}
 			
 		});
-				
-				
+		*/					
 		buPanel.add(TB);
 		buPanel.setBorder(new TitledBorder(new EtchedBorder(),"Data Input"));
 		//JPanel mainPanel= new JPanel(new MigLayout("debug ,fill","","[grow][grow][grow]"));
